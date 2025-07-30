9e44e98a245c8ad90f71db7e584056cb301e062f
mariadenny <151863215+mariadenny@users.noreply.github.com>
2025-07-30 19:54:33 +0530

    add transaction ui_1

---
+++ lib/screens/add_transaction.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class add_transaction extends StatefulWidget {
+  const add_transaction({super.key});
+
+  @override
+  State<add_transaction> createState() => _add_transactionState();
+}
+
+class _add_transactionState extends State<add_transaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        SizedBox: BoxConstraints()
+      )
+    );
+  }
+}
